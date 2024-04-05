0fad5be09e754fb1f118b4134657a05babf43ce7
isackmwangasu <mwangasuisack@gmail.com>
2024-04-05 09:06:45 +0300

    chat

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  return customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+}
+
+setBestCustomer()
+console.log(bestCustomer)
+
+overwriteBestCustomer()
+console.log(bestCustomer)
+
+const leastFavoriteCustomer = 'someone'
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else' 
+}
+
+changeLeastFavoriteCustomer()
